396c56c2aba7a058c5132b0ebba3d3aba31a6f66
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-10-15 11:47:06 -0500

    created stats

---
+++ Models/Monsters.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetSpike.Models
+{
+    public class Monsters
+    {
+        public int Id {get; set;}
+
+        public string Name {get; set;} = "T-Rex";
+
+        public int HitPoints {get; set;} = 100;
+
+        public int Attack {get; set;} = 10;
+
+        public int Defence {get; set;} = 10;
+        
+        // public MonsterType class {get; set;} = MonsterType.fire
+    }
+}
